a41d605e02ed65fbb4da0ec2a8c2fe6ccfb2dac5
bin2812 <lquanghuy218@gmail.com>
2025-03-22 10:29:43 +0700

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import TaskForm from "./TaskForm";
-
+import TaskList from "./TaskList";
 function App() {
   return (
-    <div className="bg-[#302b70]">
+    <div className="bg-[#302b70] w-full min-h-screen flex items-center justify-center">
       <div className="bg-white px-4 py-8 rounded-lg">
         <h1>To-Do List 📝</h1>
         <TaskForm />
